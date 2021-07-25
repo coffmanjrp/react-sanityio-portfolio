9732f50c6289e9a57ed87e4d6dc322e3575b38b3
coffmanjrp <coffmanjrp@gmail.com>
2021-07-25 00:57:38 -0700

    Set react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home, About, SinglePost, Post, Project } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/post/:slug" component={SinglePost} />
+        <Route exact path="/post" component={Post} />
+        <Route exact path="/project" component={Project} />
+      </Switch>
+    </Router>
   );
 }
 
