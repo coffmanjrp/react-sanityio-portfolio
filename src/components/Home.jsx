4f745be43dc656ab6849da1abc59bb8439c2a241
coffmanjrp <coffmanjrp@gmail.com>
2021-07-25 18:44:17 -0700

    Add contents to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,20 @@
+import bgImage from '../img/bg.jpg';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <img
+        src={bgImage}
+        alt="Background"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg: leading-snug home-name">
+          Hello. I'm coffmanjrp.
+        </h1>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
